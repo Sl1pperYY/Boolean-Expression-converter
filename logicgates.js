53d7b876b37af4c0612dd90c6511a190efe9d27c
Balazs Fabian <balazsfabian@bsnstudent.nl>
2019-03-25 13:43:04 +0100

    Gate functions

--- logicgates.js
+++ logicgates.js
@@ -2,30 +2,53 @@
 // Logic Gates
 // ==========================================================
 
+// AND Gate function
 function andGate(input1, input2) {
-  // filler
+  if (input1 == 1 && input2 == 1) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
+// OR Gate function
 function orGate(input1, input2) {
-  // filler
+  if (input1 == 1 || input2 == 1) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
+// NOT Gate function
 function notGate(input) {
-  // filler
+  if (input == 1) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
 
+// XOR Gate function
 function xorGate(input1, input2) {
-  // filler
+  if (input1 == input2) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
 
+// NAND gate function using notGate() and andGate()
 function nandGate(input1, input2) {
-  // filler
+  return notGate(andGate(input1, input2));
 }
 
+// NOR gate function using notGate() and orGate()
 function norGate(input1, input2) {
-  // filler
+  return notGate(orGate(input1, input2));
 }
 
-function xnokrGate(input1, input2) {
-  // filler
+// XNOR gate function using notGate() and xorGate()
+function xnorGate(input1, input2) {
+  return notGate(xorGate(input1, input2));
 }
