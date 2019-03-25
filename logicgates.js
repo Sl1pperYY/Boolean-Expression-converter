e469e47eacf895890969b6f75c15a0318e3e714a
Balazs Fabian <32176695+Sl1pperYY@users.noreply.github.com>
2019-03-25 12:25:04 +0100

    Creating logicgates.js

---
+++ logicgates.js
@@ -0,0 +1,31 @@
+// ==========================================================
+// Logic Gates
+// ==========================================================
+
+function andGate(input1, input2) {
+  // filler
+}
+
+function orGate(input1, input2) {
+  // filler
+}
+
+function notGate(input) {
+  // filler
+}
+
+function xorGate(input1, input2) {
+  // filler
+}
+
+function nandGate(input1, input2) {
+  // filler
+}
+
+function norGate(input1, input2) {
+  // filler
+}
+
+function xnokrGate(input1, input2) {
+  // filler
+}
